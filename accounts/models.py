7675cec6ec4d0bf4bfee7510f65ffee3d2ce0380
ahmet <ahmetcankaraagaclii@hotmail.com>
2019-09-06 21:53:50 +0300

    login register ve dashboard templateleri oluşturuldu

--- accounts/models.py
+++ accounts/models.py
@@ -6,15 +6,15 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     birth_date          = models.DateField(null=True, blank=True)
-    blogname		    = models.CharField(max_length=30)
-    description         = models.TextField(blank=True)
-    about               = models.TextField(blank=True)
+    blogname		    = models.CharField(max_length=30,null=True, blank=True)
+    description         = models.TextField(null=True, blank=True)
+    about               = models.TextField(null=True, blank=True)
     articles            = ArrayField(models.IntegerField(null=True, blank=True),null=True,blank=True)
     follower            = ArrayField(models.IntegerField(null=True, blank=True),null=True,blank=True)
     followed            = ArrayField(models.IntegerField(null=True, blank=True),null=True,blank=True)
     liked_articles      = ArrayField(models.IntegerField(null=True, blank=True),null=True,blank=True)
     disliked_articles   = ArrayField(models.IntegerField(null=True, blank=True),null=True,blank=True)
-    photo               = models.ImageField(upload_to='profile_photos/%Y/%m/%d/')
+    photo               = models.ImageField(upload_to='profile_photos/%Y/%m/%d/',null=True, blank=True)
 
     def __str__(self):
         return self.email
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 
 # Create your views here.
 
@@ -6,10 +10,71 @@ def dashboard(request):
     return render(request, 'accounts/dashboard.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        
+        # Get form values
+        password = request.POST['password']
+        username = request.POST['username']
+
+
+        user = auth.authenticate(username=username, password=password,)
+
+        if user is not None:
+            auth.login(request, user)
+            #messages.success(request, ' You are now logged in')
+            return redirect('accounts:dashboard')
+        else:
+            #messages.error(request, 'Invalid credentials')
+            return redirect('accounts:login')
+    else:
+        return render(request, 'accounts/login.html')
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        
+        # Get form values
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        print(username,email,password,password2)
+
+        # Check if password match
+        if password == password2:
+            print("girdi1")
+            #Check username
+            User = get_user_model()
+            if User.objects.filter(username = username).exists():
+                #messages.error(request,'This username is taken')
+                return redirect('accounts:register')
+            else:
+                if User.objects.filter(email = email).exists():
+                    #messages.error(request,'This email is being used')
+                    return redirect('accounts:register')
+
+                else:
+
+                    #looks good
+                    user = User.objects.create_user(username=username, password=password, email=email)
+                    #login after register
+                    #auth.login(request,user)
+                    #messages.success(request, 'You are now logged in')
+                    #return redirect('index')  
+                    user.save()
+                    #messages.success(request, ' You are now registered')
+                    return redirect('accounts:login')
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('accounts:register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def logout(request):
-    return redirect('index')
+    if request.method == "POST":
+        auth.logout(request)
+        #messages.success(request, 'You are now logged out')
+        return redirect('pages:index')
+
+    return redirect('pages:index')
